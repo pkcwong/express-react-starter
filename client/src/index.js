290c209d3655c7a34404fadb83b30f3d76d41673
Christopher Wong <christopherpk1996@gmail.com>
2018-11-19 17:02:00 +0800

    router history

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { Navigator } from './pages/navigator';
 
 ReactDOM.render((
-	<App/>
+	<Navigator/>
 ), document.getElementById('root'));
