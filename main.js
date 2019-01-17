4c7fd88de1aa9fe1be0cd33ccc3a33f89c833926
Wong Ping Kin Christopher <christopherpk1996@gmail.com>
2019-01-17 11:12:50 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,4 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
-	console.log('Listening on port ${port}');
-});
+app.listen(port);
